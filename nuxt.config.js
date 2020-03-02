e3e9b83757c3e49065314662bbfab839830c9013
Marc Pérez Martí <perezmarti.marc@gmail.com>
2020-03-02 10:50:05 +0100

    added sitemap generator

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,58 +1,73 @@
-const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
-  router: {
-    base: '/'
-  }
-} : {}
+const routerBase =
+  process.env.DEPLOY_ENV === "GH_PAGES"
+    ? {
+        router: {
+          base: "/"
+        }
+      }
+    : {};
 
 export default {
-  mode: 'universal',
+  mode: "universal",
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+
+  sitemap: {
+    hostname: 'https://getsilt.com',
+    path: "/sitemap.xml",
+    cacheTime: 1000 * 60 * 60 * 2,
+    trailingSlash: true,
+    gzip: true
+  },
+
+  loading: { color: "#fff" },
   /*
-  ** Global CSS
-  */
-  css: [
-    '@/assets/sass/main.sass'
-  ],
+   ** Global CSS
+   */
+  css: ["@/assets/sass/main.sass"],
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-    { src: '~/plugins/scrollmagic', ssr: false }
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [{ src: "~/plugins/scrollmagic", ssr: false }],
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
-    ['nuxt-i18n',
+    [
+      "nuxt-i18n",
       {
         detectBrowserLanguage: {
           useCookie: true,
-          cookieKey: 'i18n_redirected'
+          cookieKey: "i18n_redirected"
         }
-      }],
-      ['nuxt-svg'],
-      ['@nuxtjs/google-analytics', {
-        id: 'UA-144041578-1'
-      }]
+      }
+    ],
+    '@nuxtjs/sitemap',
+    ["nuxt-svg"],
+    [
+      "@nuxtjs/google-analytics",
+      {
+        id: "UA-144041578-1"
+      }
+    ]
   ],
-
 
   /*
   * add this to activate spanish translation
@@ -64,21 +79,20 @@ export default {
   * 
   */
 
-
   i18n: {
     locales: [
       {
-        name: 'English',
-        code: 'en',
-        iso: 'en-US',
+        name: "English",
+        code: "en",
+        iso: "en-US"
       }
     ],
-    defaultLocale: 'en',
+    defaultLocale: "en",
     vueI18n: {
-      fallbackLocale: 'en',
+      fallbackLocale: "en",
       messages: {
-        en: require('./lang/en-US.json'),
-        es: require('./lang/es-ES.json'),
+        en: require("./lang/en-US.json"),
+        es: require("./lang/es-ES.json")
       },
       silentTranslationWarn: false
     }
@@ -88,31 +102,29 @@ export default {
     fallback: "404.html"
   },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-    },
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {},
     postcss: {
-    // Add plugin names as key and arguments as value
-    // Install them before as dependencies with npm or yarn
-    plugins: {
-      // Disable a plugin by passing false as value 
-      'postcss-url': false,
-      'postcss-nested': {},
-      'postcss-responsive-type': {},
-      'postcss-hexrgba': {}
-    },
-    preset: {
-      // Change the postcss-preset-env settings
-      autoprefixer: {
-        grid: true
+      // Add plugin names as key and arguments as value
+      // Install them before as dependencies with npm or yarn
+      plugins: {
+        // Disable a plugin by passing false as value
+        "postcss-url": false,
+        "postcss-nested": {},
+        "postcss-responsive-type": {},
+        "postcss-hexrgba": {}
+      },
+      preset: {
+        // Change the postcss-preset-env settings
+        autoprefixer: {
+          grid: true
+        }
       }
     }
   }
-
-  }
-}
+};
