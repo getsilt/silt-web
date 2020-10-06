51dd4998974b18a3155e8863edb81b08e20bfff4
Marc Pérez Martí <perezmarti.marc@gmail.com>
2020-10-06 20:13:52 +0200

    onlyOnRoot true

--- nuxt.config.js
+++ nuxt.config.js
@@ -75,7 +75,7 @@ export default {
         detectBrowserLanguage: {
           useCookie: true,
           cookieKey: "i18n_redirected",
-          onlyOnRoot: false
+          onlyOnRoot: true
         }
       }
     ],
