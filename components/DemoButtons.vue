e22c8adb8b1b13c7d5582c72ca2c1ee1fbbc1b0a
Gerard Albajar <gerard.albajar@gmail.com>
2022-11-17 12:56:04 +0100

    Adding .demo-button class at DemoButtons.vue

--- components/DemoButtons.vue
+++ components/DemoButtons.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="demo_cta__wrapper">
     <a href="https://dashboard.getsilt.com/welcome">
-      <button>{{ $t("btn_cta_demo") }}</button>
+      <button class="demo-button">{{ $t("btn_cta_demo") }}</button>
     </a>
     <nuxt-link class="bold-link demo-link" :to="localePath({ name: 'demo' })">
       {{ $t("btn_cta_book_demo") }}
