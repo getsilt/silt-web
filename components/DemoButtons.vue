559ede91e2cfc346006b011f02f6fae809e0e8c1
Gerard Albajar <gerard.albajar@gmail.com>
2022-11-29 18:02:22 +0100

    Updating url DemoButtons

--- components/DemoButtons.vue
+++ components/DemoButtons.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="demo_cta__wrapper">
-    <a href="https://dashboard.getsilt.com/welcome">
-      <button class="demo-button">{{ $t("btn_cta_demo") }}</button>
+    <a target="_blank" href="https://dashboard.getsilt.com/welcome">
+      <button>{{ $t("btn_cta_demo") }}</button>
     </a>
-    <nuxt-link class="bold-link demo-link" :to="localePath({ name: 'demo' })">
-      {{ $t("btn_cta_book_demo") }}
-    </nuxt-link>
+    <a
+      target="_blank"
+      class="bold-link demo-link"
+      href="https://app.meetzy.io/preview/3dfd0d2hgymu"
+      >{{ $t("btn_cta_book_demo") }}</a
+    >
   </div>
 </template>
 
