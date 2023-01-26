1fd3fc3db882e1e942ce7d8d853386f68afc7393
Gerard Albajar <gerard.albajar@gmail.com>
2023-01-26 13:11:55 +0100

    Adding solution.vue at rules folder

---
+++ pages/rules/solution.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="fw-container--dark">
+    <section class="container-md">
+      <div class="section-headers">
+        <span class="tag">{{ $t("business_solution_section") }}</span>
+        <h2>{{ $t("business_solution_rules_page_title") }}</h2>
+        <p>{{ $t("business_solution_rules_page_subtitle") }}</p>
+      </div>
+      <div class="integration-container">
+        <div class="card">
+          <!-- TODO: Add New schema -->
+          <div class="article-container">
+            <h4>{{ $t("business_solution_rules_page_content1") }}</h4>
+            <p>{{ $t("business_solution_rules_page_content2") }}</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import gsap from "gsap";
+export default {
+  data() {
+    return {
+      email: "hello@getsilt.com",
+    };
+  },
+  mounted() {
+    this.startFirstAnimationScene(this);
+  },
+  methods: {
+    getEmail() {
+      return this.email;
+    },
+    startFirstAnimationScene: (_this) => {
+      const sections = ["#solution"];
+      let tm = {};
+      for (let section of sections) {
+        const tl = gsap.timeline({
+          scrollTrigger: {
+            trigger: section,
+            start: "top 80%",
+            scrub: false,
+            markers: false,
+          },
+        });
+        tl.from(section, {
+          duration: 1,
+          scale: 1,
+          y: 100,
+          opacity: 0,
+        });
+      }
+    },
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+@import "@/assets/sass/vars.sass"
+.integration-container
+  display: flex
+  &>*
+    flex: 1 1 50%
+    margin-right: $spacing-md
+  &>*:last-child
+    margin-right: 0
+
+.integration-link
+    margin-top: $spacing-xlg
+    .link-with-arrow
+      position: relative
+      &:after, &:before
+        transition: 0.2s ease-in-out
+        opacity: 0
+        transform: translateX(0%)
+        position: absolute
+        right: 0
+        font-family: "Font Awesome 5 Duotone"
+        content: "\f061"
+        color: #2963ff
+      &:hover:after
+        transition: 0.2s ease-in-out
+        opacity: 1
+        transform: translateX(100%)
+</style>
