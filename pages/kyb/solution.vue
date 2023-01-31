e375d4db74be85c141bdda26c4e188e2e1d686e6
Gerard Albajar <gerard.albajar@gmail.com>
2023-01-31 11:02:29 +0100

    Adding solution.vue at kyb folder

---
+++ pages/kyb/solution.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="fw-container--dark">
+    <section class="container-md">
+      <div class="section-headers">
+        <span class="tag">{{ $t("business_solution_section") }}</span>
+        <h2>{{ $t("kyb_solution_title") }}</h2>
+        <p>{{ $t("kyb_solution_content1") }}</p>
+      </div>
+      <div class="card">
+        <div class="article-container claim-container">
+          <div class="claim-side-img screenshots-container">
+            <template v-if="$root.$i18n.locale === 'es'">
+              <img
+                src="@/assets/img/illustrations/kyb_flow_es.svg"
+                :alt="$t('seo_3')"
+                width="300"
+              />
+            </template>
+            <template v-else>
+              <img
+                src="@/assets/img/illustrations/kyb_flow_en.svg"
+                :alt="$t('seo_3')"
+                width="300"
+              />
+            </template>
+          </div>
+          <div class="claim-info top-claim-info">
+            <p>{{ $t("kyb_solution_content2") }}</p>
+            <p>
+              <b>{{ $t("kyb_solution_compliant") }}</b>
+            </p>
+            <p class="centered-block integration-link">
+              <a
+                target="_blank"
+                href="https://dashboard.getsilt.com/welcome"
+                class="bold-link link-with-arrow"
+              >
+                {{ $t("btn_cta_demo") }}
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import gsap from "gsap";
+export default {
+  data() {
+    return {
+      email: "hello@getsilt.com",
+    };
+  },
+  mounted() {
+    this.startFirstAnimationScene(this);
+  },
+  methods: {
+    getEmail() {
+      return this.email;
+    },
+    startFirstAnimationScene: (_this) => {
+      const sections = ["#solution"];
+      let tm = {};
+      for (let section of sections) {
+        const tl = gsap.timeline({
+          scrollTrigger: {
+            trigger: section,
+            start: "top 80%",
+            scrub: false,
+            markers: false,
+          },
+        });
+        tl.from(section, {
+          duration: 1,
+          scale: 1,
+          y: 100,
+          opacity: 0,
+        });
+      }
+    },
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+@import "@/assets/sass/vars.sass"
+
+.top-claim-info
+  margin-bottom: 0
+  margin-top: $spacing-xlg
+</style>
