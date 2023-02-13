45989da5001a39c5981ba726a9d027b28a60fd4a
Gerard Albajar <gerard.albajar@gmail.com>
2023-02-13 10:43:44 +0100

    Adding AML at KYC section

--- components/MobileNav.vue
+++ components/MobileNav.vue
@@ -40,6 +40,19 @@
             </p>
           </div>
         </nuxt-link>
+        <nuxt-link
+          class="dropdown-wrap__content type1"
+          :to="localePath({ name: 'aml' })"
+          @click.native="$emit('onCloseNav')"
+        >
+          <i class="fad fa-lg fa-address-book scan-icon" />
+          <div>
+            <h6 class="title_menu">{{ $t("nav_feature_aml") }}</h6>
+            <p class="subtitle_menu">
+              {{ $t("product_subtitle_aml") }}
+            </p>
+          </div>
+        </nuxt-link>
       </div>
       <div class="dropdown--section_title">{{ $t("nav_features_other") }}</div>
       <div class="dropdown-wrap">
@@ -65,19 +78,6 @@
           <div>
             <h6 class="title_menu">{{ $t("nav_feature_kyb") }}</h6>
             <p class="subtitle_menu">{{ $t("product_subtitle_kyb") }}</p>
-          </div>
-        </nuxt-link>
-        <nuxt-link
-          class="dropdown-wrap__content type1"
-          :to="localePath({ name: 'aml' })"
-          @click.native="$emit('onCloseNav')"
-        >
-          <i class="fad fa-lg fa-address-book scan-icon" />
-          <div>
-            <h6 class="title_menu">{{ $t("nav_feature_aml") }}</h6>
-            <p class="subtitle_menu">
-              {{ $t("product_subtitle_aml") }}
-            </p>
           </div>
         </nuxt-link>
       </div>
