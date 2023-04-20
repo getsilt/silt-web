109a6e775f22eca12dff2ef99b561a4ad11a781c
Gerard Albajar <gerard.albajar@gmail.com>
2023-04-20 13:38:01 +0200

    Updating faq.vue adding copies and expandible component

--- pages/faq.vue
+++ pages/faq.vue
@@ -3,10 +3,42 @@
     <div class="section-headers">
       <h1>{{ $t("FAQ_title_2") }}</h1>
     </div>
+    <h3 class="faq-headers">{{ $t("FAQ_subtitle_general") }}</h3>
+    <div class="card">
+      <expandable :key="`generalFAQ-${i}`" v-for="(faq, i) of generalFAQs">
+        <h6 v-html="$t(faq.title)" slot="title" />
+        <div slot="body">
+          {{ $t(faq.body) }}
+        </div>
+      </expandable>
+    </div>
+    <h3 class="faq-headers">{{ $t("FAQ_subtitle_eID_and_integration") }}</h3>
+    <div class="card">
+      <expandable :key="`eIDFAQ-${i}`" v-for="(faq, i) of eIDFAQs">
+        <h6 v-html="$t(faq.title)" slot="title" />
+        <div slot="body">
+          {{ $t(faq.body) }}
+        </div>
+      </expandable>
+    </div>
+
+    <h3 class="faq-headers">{{ $t("FAQ_subtitle_integration") }}</h3>
+    <div class="card">
+      <expandable
+        :key="`integrationFAQ-${i}`"
+        v-for="(faq, i) of integrationFAQs"
+      >
+        <h6 v-html="$t(faq.title)" slot="title" />
+        <div slot="body">
+          {{ $t(faq.body) }}
+        </div>
+      </expandable>
+    </div>
   </div>
 </template>
 
 <script>
+import Expandable from "@/components/Expandable.vue";
 export default {
   nuxtI18n: {
     paths: {
@@ -37,9 +69,76 @@ export default {
       ],
     };
   },
+  components: { Expandable },
   data() {
     return {
       email: "hello@getsilt.com",
+      generalFAQs: [
+        {
+          title: "FAQ_what_is _identity_verification_title",
+          body: "FAQ_what_is _identity_verification_body",
+        },
+        {
+          title: "FAQ_sectors_identity_verification_title",
+          body: "FAQ_sectors_identity_verification_body",
+        },
+        {
+          title: "FAQ_countries_and_languages_supported_title",
+          body: "FAQ_countries_and_languages_supported_body",
+        },
+        {
+          title: "FAQ_contact_Silt_title",
+          body: "FAQ_contact_Silt_body",
+        },
+      ],
+      eIDFAQs: [
+        {
+          title: "FAQ_difference_KYC_KYB_title",
+          body: "FAQ_difference_KYC_KYB_body",
+        },
+        {
+          title: "FAQ_biocheck_secure_title",
+          body: "FAQ_biocheck_secure_body",
+        },
+        {
+          title: "FAQ_time_identity_verification_title",
+          body: "FAQ_time_identity_verification_body",
+        },
+        {
+          title: "FAQ_how_identity_verification_technology_title",
+          body: "FAQ_how_identity_verification_technology_body",
+        },
+        {
+          title: "FAQ_regulations_compliance_title",
+          body: "FAQ_regulations_compliance_body",
+        },
+        {
+          title: "FAQ_data_storage_title",
+          body: "FAQ_data_storage_body",
+        },
+        {
+          title: "FAQ_user_needs automatic_verification_title",
+          body: "FAQ_user_needs automatic_verification_body",
+        },
+        {
+          title: "FAQ_contact_Silt_title",
+          body: "FAQ_contact_Silt_body",
+        },
+      ],
+      integrationFAQs: [
+        {
+          title: "FAQ_integration_with_Silt_title",
+          body: "FAQ_integration_with_Silt_body",
+        },
+        {
+          title: "FAQ_time_integration_with_Silt_title",
+          body: "FAQ_time_integration_with_Silt_body",
+        },
+        {
+          title: "FAQ_link_clients_CRM_title",
+          body: "FAQ_link_clients_CRM_body",
+        },
+      ],
     };
   },
   methods: {
@@ -51,4 +150,13 @@ export default {
 </script>
 
 <style lang="sass" scoped>
+@import "@/assets/sass/vars.sass"
+
+.section-headers
+    margin-bottom: 0
+
+.faq-headers
+    margin: $spacing-md 0
+.card
+    text-align: left
 </style>
