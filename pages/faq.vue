cc3bae1f3178c709ced16017c906450f70a9d3de
Gerard Albajar <gerard.albajar@gmail.com>
2023-04-20 11:18:14 +0200

    Adding faq.vue at pages folder

---
+++ pages/faq.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="fw-container container" id="faq">
+    <div class="section-headers">
+      <h1>{{ $t("FAQ_title") }}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  nuxtI18n: {
+    paths: {
+      en: "/faq", // -> accessible at / (no prefix since it's the default locale)
+      es: "/faq",
+    },
+  },
+  head() {
+    const i18nSeo = this.$nuxtI18nHead();
+    return {
+      title: this.$t("FAQ_title"),
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: this.$t("business_claim2"),
+        },
+        {
+          hid: "og:description",
+          name: "og:description",
+          content: this.$t("business_claim2"),
+        },
+        {
+          hid: "og:title",
+          name: "og:title",
+          content: this.$t("nav_feature_aml"),
+        },
+      ],
+    };
+  },
+  data() {
+    return {
+      email: "hello@getsilt.com",
+    };
+  },
+  methods: {
+    getEmail() {
+      return this.email;
+    },
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+</style>
