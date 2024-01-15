17195af591853e01e14cc2ee06e4079a7aff8a5c
Gerard Albajar <gerard.albajar@gmail.com>
2024-01-15 13:15:31 +0100

    Improve TrustOf.vue styles and logic

--- components/TrustOf.vue
+++ components/TrustOf.vue
@@ -3,43 +3,16 @@
     <h6 class="seo_header">{{ $t("footer_trust") }}</h6>
     <div class="trust__logos">
       <ul class="trust__logos_list">
-        <li class="trust__logos_item">
-          <img src="@/assets/img/logo/gobierno_de_españa.svg.png" alt="" />
-        </li>
-        <li class="trust__logos_item">
-          <img src="@/assets/img/logo/tech_bcn.png" alt="" />
-        </li>
-        <li class="trust__logos_item">
-          <img src="@/assets/img/logo/accio.png" alt="" />
-        </li>
-        <li class="trust__logos_item">
-          <img src="@/assets/img/logo/deloitte.svg.png" alt="" />
-        </li>
-        <li class="trust__logos_item">
-          <img src="@/assets/img/logo/glovo.svg.png" alt="" />
-        </li>
-        <li class="trust__logos_item">
+        <li
+          class="trust__logos_item"
+          v-for="clientImage of clientsImages"
+          :key="clientImage.scr"
+        >
           <img
-            style="padding: 20px"
-            src="@/assets/img/logo/nester_software.svg"
-            alt=""
+            :style="clientImage.style"
+            :src="require(`@/assets/img/logo/${clientImage.src}`)"
+            :alt="clientImage.alt"
           />
-        </li>
-        <li class="trust__logos_item">
-          <img src="@/assets/img/logo/offugo.png" alt="" />
-        </li>
-        <li class="trust__logos_item">
-          <img src="@/assets/img/logo/aefi.png" alt="" />
-        </li>
-        <li class="trust__logos_item">
-          <img
-            style="padding: 10px"
-            src="@/assets/img/logo/plan_recuperacion.png"
-            alt=""
-          />
-        </li>
-        <li class="trust__logos_item">
-          <img src="@/assets/img/logo/recoletas.svg" alt="" />
         </li>
       </ul>
     </div>
@@ -47,29 +20,78 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      clientsImages: [
+        {
+          src: "gobierno_de_españa.svg.png",
+          alt: "gobierno_de_españa",
+          style: "filter: contrast(100%) grayscale(1)",
+        },
+        {
+          src: "tech_bcn.png",
+          alt: "tech_bcn",
+          style: "",
+        },
+        { src: "accio.png", alt: "accio", style: "" },
+        {
+          src: "deloitte.svg.png",
+          alt: "deloitte.svg",
+          style: "",
+        },
+        {
+          src: "glovo.svg.png",
+          alt: "glovo.svg",
+          style: "",
+        },
+        {
+          src: "nester_software.svg",
+          alt: "nester_software",
+          style: "padding: 20px",
+        },
+        { src: "offugo.png", alt: "offugo", style: "" },
+        { src: "aefi.png", alt: "aefi", style: "" },
+        {
+          src: "plan_recuperacion.png",
+          alt: "plan_recuperacion",
+          style: "padding: 10px",
+        },
+        {
+          src: "recoletas.svg",
+          alt: "recoletas",
+          style: "",
+        },
+      ],
+    };
+  },
+  methods: {
+    cloneAndAppendItems() {
+      const logosList = document.querySelector(".trust__logos_list");
+      const items = logosList.querySelectorAll(".trust__logos_item");
+
+      items.forEach((item) => {
+        const clone = item.cloneNode(true);
+        logosList.appendChild(clone);
+      });
+    },
+  },
+  mounted() {
+    this.cloneAndAppendItems();
+  },
+};
 </script>
 
 <style scoped lang="sass">
 @import "@/assets/sass/vars.sass"
-
-@keyframes slide-then-hide
-  0%
-    transform: translate(0, 0)
-
-  100%
-    display: none
-    transform: translate(-100%, 0)
-
-@keyframes slide
-  0%
-    transform: translate(0, 0)
-
-  100%
-    transform: translate(-100%, 0)
-
 .seo_header
   filter: contrast(5%) grayscale(1)
+
+@keyframes infiniteScroll
+  from
+    transform: translateX(0)
+  to
+    transform: translateX(calc(0px - 50% - 500px))
 
 .trust__wrapper
   text-align: center
@@ -91,25 +113,18 @@ export default {};
     overflow: hidden
     .trust__logos_list
       display: flex
-      width: 100%
-      height: 100%
-      max-width: 100%
-      max-height: 100%
-      place-items: center
-      margin: 0
-      padding: 0
-      list-style-type: none
-      text-indent: none
-      gap: 48px
-      position: relative
-      flex-direction: row
-      will-change: transform
-      transform: translateX(0px)
-      animation-name: slide
-      animation-duration: 40s
-      animation-timing-function: linear
+      font-size: 40px
+      gap: $spacing-xlg
+      width: 2600px
+      animation-name: infiniteScroll
+      animation-duration: 20s
       animation-iteration-count: infinite
+      animation-timing-function: linear
+      list-style: none
       .trust__logos_item
+        height: 100%
+        width: 200px
+        background-size: cover
         img
           filter: contrast(5%) grayscale(1)
           margin: 10px
