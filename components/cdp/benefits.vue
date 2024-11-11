c90f80801b8269a11f2fc012aca5fe18805b5a5b
Gerard Albajar <gerard.albajar@gmail.com>
2024-11-11 13:35:38 +0100

    Add benefits.vue at cdp folder

---
+++ components/cdp/benefits.vue
@@ -0,0 +1,85 @@
+<template>
+  <section class="fw-container">
+    <div class="highlights-wrapper">
+      <div class="section-headers">
+        <span class="tag">{{ $t("business_benefits_section") }}</span>
+        <h2 v-html="$t('cdp_benefit_title')" />
+        <p class="subtitle" v-html="$t('cdp_benefit_subtitle')" />
+      </div>
+      <div class="list-wrapper pins">
+        <card
+          v-for="benefit in benefits"
+          :key="benefit.title"
+          class="card--w-sm"
+        >
+          <div class="article-container" slot="body">
+            <h4 v-html="$t(`${benefit.title}`)" class="color_primary_text" />
+            <p>{{ $t(benefit.content) }}</p>
+          </div>
+        </card>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import gsap from "gsap";
+import Card from "@/components/Card.vue";
+
+export default {
+  components: { Card },
+  data() {
+    return {
+      email: "hello@getsilt.com",
+      benefits: [
+        {
+          title: "cdp_benefit_time_title",
+          content: "cdp_benefit_time_body",
+        },
+        {
+          title: "cdp_benefit_growth_title",
+          content: "cdp_benefit_growth_body",
+        },
+        {
+          title: "cdp_benefit_precision_title",
+          content: "cdp_benefit_precision_body",
+        },
+        {
+          title: "cdp_benefit_satisfaction_title",
+          content: "cdp_benefit_satisfaction_body",
+        },
+      ],
+    };
+  },
+  mounted() {
+    this.startFirstAnimationScene(this);
+  },
+  methods: {
+    getEmail() {
+      return this.email;
+    },
+    startFirstAnimationScene: (_this) => {
+      const sections = ["#benefits"];
+      let tm = {};
+      for (let section of sections) {
+        const tl = gsap.timeline({
+          scrollTrigger: {
+            trigger: section,
+            start: "top 80%",
+            scrub: false,
+            markers: false,
+          },
+        });
+        tl.from(section, {
+          duration: 1,
+          scale: 1,
+          y: 100,
+          opacity: 0,
+        });
+      }
+    },
+  },
+};
+</script>
+
+<style lang="sass" scoped></style>
