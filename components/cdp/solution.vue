0134dc5544e5bba8091882513ba759b36c1e06c2
Gerard Albajar <gerard.albajar@gmail.com>
2024-11-11 16:22:22 +0100

    Update solution.vue from cdp folder and add cdp.vue

--- components/cdp/solution.vue
+++ components/cdp/solution.vue
@@ -8,20 +8,34 @@
         </span>
       </h2>
       <p class="subtitle" v-html="$t('cdp_solution_subtitle')" />
-      <Card class="card--w-sm">
-        <div class="article-container" slot="body">
-          <h2 class="color_primary_text"><b>99%</b></h2>
-          <p>{{ $t("cdp_solution_body_claim") }}</p>
+      <Card class="card">
+        <div class="article-container solution_wrapper" slot="body">
+          <div>
+            <h2 class="color_accent_text"><b>99%</b></h2>
+            <h3 class="color_accent_text">
+              <b>{{ $t("global_accuracy") }}</b>
+            </h3>
+          </div>
+          <span>
+            {{ $t("cdp_solution_accuracy_body") }}
+          </span>
         </div>
       </Card>
+      <div class="solution_wrapper">
+        <h4 class="solution_wrapper__book-a-demo">
+          {{ $t("book_a_demo_cdp") }}
+        </h4>
+        <DemoButtons :dark="true" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import Card from "../Card.vue";
+import DemoButtons from "../DemoButtons.vue";
 export default {
-  components: { Card },
+  components: { Card, DemoButtons },
   data() {
     return {};
   },
@@ -32,4 +46,20 @@ export default {
 
 <style lang="sass" scoped>
 @import "@/assets/sass/vars.sass"
+
+h2,h3
+  margin-bottom: 0
+
+.card
+  margin-bottom: $spacing-xlg
+
+.solution_wrapper
+  display: flex
+  flex-direction: row
+  gap: $spacing-xlg
+  text-align: left
+  justify-content: space-between
+  width: 100%
+  &__book-a-demo
+    max-width: 400px
 </style>
---
+++ pages/cdp.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <hero />
+    <TrustOf />
+    <Benefits />
+    <Solution />
+  </div>
+</template>
+
+<script>
+import gsap from "gsap";
+import Hero from "@/components/cdp/hero.vue";
+import TrustOf from "../components/TrustOf.vue";
+import Benefits from "../components/cdp/benefits.vue";
+import Solution from "../components/cdp/solution.vue";
+import DemoButtons from "../components/DemoButtons.vue";
+
+export default {
+  components: {
+    Hero,
+    TrustOf,
+    Benefits,
+    Solution,
+    DemoButtons,
+  },
+  head() {
+    const i18nSeo = this.$nuxtI18nHead();
+    return {
+      title: this.$t("seo_cdp_title"),
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: this.$t("seo_cdp_metadescription"),
+        },
+        {
+          hid: "og:title",
+          name: "og:title",
+          content: this.$t("seo_cdp_metadescription"),
+        },
+        {
+          hid: "og:description",
+          name: "og:description",
+          content: this.$t("seo_cdp_title"),
+        },
+      ],
+    };
+  },
+  data() {
+    return {
+      email: "hello@getsilt.com",
+    };
+  },
+  mounted() {
+    this.startFirstAnimationScene(this);
+  },
+  methods: {
+    getEmail() {
+      return this.email;
+    },
+    startFirstAnimationScene: (_this) => {
+      const sections = ["#problem", "#solution", "#benefits"];
+      let tm = {};
+      for (let section of sections) {
+        const tl = gsap.timeline({
+          scrollTrigger: {
+            trigger: section,
+            start: "top 80%",
+            scrub: false,
+            markers: false,
+          },
+        });
+        tl.from(section, {
+          duration: 1,
+          scale: 1,
+          y: 100,
+          opacity: 0,
+        });
+      }
+    },
+  },
+};
+</script>
+
+<style lang="sass" scoped></style>
