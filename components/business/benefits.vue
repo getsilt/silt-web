624c28d17e3070409fc36e4f170fbe4050e15f85
Gerard Albajar <gerard.albajar@gmail.com>
2025-03-17 12:08:22 +0100

    Update benefits.vue from business folder

--- components/business/benefits.vue
+++ components/business/benefits.vue
@@ -18,7 +18,9 @@
             class="card--w-sm"
           >
             <div class="article-container" slot="body">
-              <h1 class="colored">{{ benefit.kpi }}</h1>
+              <span class="colored article-container-kpi">
+                {{ benefit.kpi }}
+              </span>
               <h4 class="colored">{{ $t(`${benefit.title}`) }}</h4>
               <p>{{ $t(benefit.content) }}</p>
             </div>
@@ -101,7 +103,9 @@ export default {
   margin-top: $spacing-sm
   margin-bottom: $spacing-lg
 .article-container
-  h1
+  &-kpi
+    font-size: 3.2rem
+    font-family: $font-bold
     margin-bottom: $spacing-sm
 
 .benefits-demo-buttons
